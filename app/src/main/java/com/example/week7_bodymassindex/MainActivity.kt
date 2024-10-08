225e86fbf27217d49af23188d69f669fd54e055c
Jempsa <t2koje02@students.oamk.fi>
2024-10-08 15:37:18 +0300

    Valmis

---
+++ app/src/main/java/com/example/week7_bodymassindex/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.week7_bodymassindex
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.week7_bodymassindex.ui.theme.Week7_BodyMassIndexTheme
+
+class MainActivity : ComponentActivity() {
+    private val viewModel: BmiViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Week7_BodyMassIndexTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Bmi(viewModel)
+                }
+            }
+        }
+    }
+}
+
+@SuppressLint("DefaultLocale")
+@Composable
+fun Bmi(viewModel: BmiViewModel) {
+    Column {
+        Text(
+            text = stringResource(R.string.body_mass_index),
+            fontSize = 24.sp,
+            color = MaterialTheme.colorScheme.primary,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp, bottom = 16.dp)
+        )
+        OutlinedTextField(
+            value = viewModel.heightInput,
+            onValueChange = { viewModel.onHeightChanged(it) },
+            label = { Text(stringResource(R.string.height)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = viewModel.weightInput,
+            onValueChange = { viewModel.onWeightChanged(it) },
+            label = { Text(stringResource(R.string.weight)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            text = stringResource(R.string.result, String.format("%.2f", viewModel.bmi).replace(',', '.')),
+            fontSize = 18.sp,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+    }
+}
